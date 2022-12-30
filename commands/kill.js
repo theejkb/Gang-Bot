16d8033f950fa8cdf329b7a608a4e8d2e7b67cf0
Steven <stevencopy@hotmail.fr>
2022-12-30 22:40:07 +0100

    Update: command deployed

--- commands/kill.js
+++ commands/kill.js
@@ -3,17 +3,23 @@ const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('kill')
-		.setDescription('Kill someone dirty!')
-        .addUserOption(option =>
-            option.setName('target')
-                .setDescription('The person to kill')
-                .setRequired(true))
-        .addStringOption(option => 
-            option.setName('reason')
-                .setDescription('Why would you kill him?')),
+		.setDescription('Kill someone.')
+		.addUserOption(option =>
+			option.setName('target')
+				.setDescription('The person to kill')
+				.setRequired(true))
+		.addStringOption(option =>
+			option.setName('reason')
+				.setDescription('Why would you kill him?')),
 	async execute(interaction) {
-        const target = interaction.options.getUser('target');
-        const reason = interaction.options.getString('reason') ?? 'No reason provided';
-		await interaction.reply(`${interaction.user.username} just killed ${target} because ${reason} 🔪`);
+		const target = interaction.options.getUser('target');
+		const reasonValue = interaction.options.getString('reason');
+
+		if (target.username === 'NaaQ'){
+			await interaction.reply(`Mhhh... Sorry but ${target} can't be killed.`);
+		} else {
+			const reason = reasonValue? 'because ' + reasonValue :'for no reason.';
+			await interaction.reply(`${interaction.user.username} just revive ${target} ${reason}`);
+		}
 	},
 };
